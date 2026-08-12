69e9177451dd718204f3ad94ccaa59d6e75610a3
SakuraAho <skurausagikun@gmail.com>
2026-08-12 15:44:08 +0800

    chore: v2.6.4 soften playbook export footer narrative guidance

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 import Bridge from './bridge.js';
 
-const TERM_VERSION = '2.6.3';
+const TERM_VERSION = '2.6.4';
 const TERM_BASE = new URL('./', import.meta.url).href;
 
 if (window.NAVI_TERM_LOADED) {
